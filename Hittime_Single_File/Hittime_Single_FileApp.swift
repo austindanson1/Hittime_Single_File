de0e144cbce898fb4922d3f5b2cb3fc025515dc8
austindanson <austindanson@gmail.com>
2023-05-20 20:31:47 -0700

    Initial Commit

---
+++ Hittime_Single_File/Hittime_Single_FileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hittime_Single_FileApp.swift
+//  Hittime_Single_File
+//
+//  Created by Austin Danson on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Hittime_Single_FileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hittime_Single_FileUITests/Hittime_Single_FileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hittime_Single_FileUITestsLaunchTests.swift
+//  Hittime_Single_FileUITests
+//
+//  Created by Austin Danson on 5/20/23.
+//
+
+import XCTest
+
+final class Hittime_Single_FileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
